f37a7c3b46c639251312aea4de1e329c4c4aa430
Alexander <lapseofreason17@gmail.com>
2019-03-09 18:51:22 +0300

    Create Exponential of a Number.scm

---
+++ Examples/Exponential of a Number.scm
@@ -0,0 +1,15 @@
+;linear recursive process
+(define (expt-recursive b n)
+ (if (= n 0)
+ 1
+ (* b (expt-recursive b (- n 1)))))
+
+;linear iterative process
+(define (expt-iterative b n)
+  (define (iter b counter product)
+   (if (= counter 0)
+   product
+   (iter b
+   (- counter 1)
+   (* b product))))
+ (iter b n 1))
