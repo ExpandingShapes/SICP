5a84d31f802d591dbe5ee26deac2355df2aca01c
Alexander <lapseofreason17@gmail.com>
2019-03-29 22:24:07 +0300

    Create Exercise 1.16.scm

---
+++ Exercises/Exercise 1.16.scm
@@ -0,0 +1,8 @@
+(define (square x) (* x x))
+(define (even? n)
+    (= (remainder n 2) 0))
+(define (fast-expt b n)
+  (let expt ((b b) (n n) (a 1))
+    (cond ((zero? n) a)
+          ((even? n) (expt (* b b) (/ n 2) a))
+          (else (expt b (- n 1) (* b a))))))
