0ae93dc934ca39469a378f2d1f231ba40e2295dc
Alexander <lapseofreason17@gmail.com>
2019-03-29 22:26:27 +0300

    Update Exercise 1.16.scm

--- Exercises/Exercise 1.16.scm
+++ Exercises/Exercise 1.16.scm
@@ -3,6 +3,6 @@
     (= (remainder n 2) 0))
 (define (fast-expt b n)
   (let expt ((b b) (n n) (a 1))
-    (cond ((zero? n) a)
+    (cond ((= n 0) a)
           ((even? n) (expt (* b b) (/ n 2) a))
           (else (expt b (- n 1) (* b a))))))
