73d185baef39e6ae6f2a0d32eeee1c776fc8c5e4
Alexander <lapseofreason17@gmail.com>
2019-04-19 22:48:45 +0300

    Create 1.3.3 Finding fixed points of functions.scm

---
+++ Examples/1.3.3 Finding fixed points of functions.scm
@@ -0,0 +1,16 @@
+(define tolerance 0.00001)
+
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(display (fixed-point cos 1.0))
+(display " ")
+(display (fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0))
