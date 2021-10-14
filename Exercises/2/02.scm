357eead4cb2b251bd5b6e7c881eb6fd084b7d277
Alexander <lapseofreason17@gmail.com>
2021-10-14 16:33:21 +0300

    Create 02.scm

---
+++ Exercises/2/02.scm
@@ -0,0 +1,25 @@
+(define (midpoint-segment segment) (make-point (/ (+ (x-point (start-segment segment))
+                                                     (x-point (end-segment segment)))
+                                                 2)
+                                               (/ (+ (y-point (start-segment segment))
+                                                     (y-point (end-segment segment)))
+                                                 2)))
+
+(define (make-segment start end) (cons start end))
+(define (start-segment segment) (car segment))
+(define (end-segment segment) (cdr segment))
+
+(define (make-point x y) (cons x y))
+(define (x-point point) (car point))
+(define (y-point point) (cdr point))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+;Test. Point (30, 15) must be returned
+(print-point (midpoint-segment (make-segment (make-point 10 20)
+                                             (make-point 50 10))))
