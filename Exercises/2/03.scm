19b83023e707548618430b14afec2097bf81e660
Alexander <lapseofreason17@gmail.com>
2021-10-17 17:38:08 +0300

    Update 03.scm

--- Exercises/2/03.scm
+++ Exercises/2/03.scm
@@ -1,5 +1,5 @@
-(define (make-rectangle upper-segment lower-segment) (cons upper-segment lower-segment))
-()
+(define (make-rectangle-1 upper-segment lower-segment) (cons upper-segment lower-segment))
+(define (make-rectangle-2 left-segment right-segment) (cons left-segment right-segment))
 
 (define (make-segment start end) (cons start end))
 (define (start-segment segment) (car segment))
@@ -9,13 +9,5 @@
 (define (x-point point) (car point))
 (define (y-point point) (cdr point))
 
-(define (print-point p)
-  (newline)
-  (display "(")
-  (display (x-point p))
-  (display ",")
-  (display (y-point p))
-  (display ")"))
-;Test. Point (30, 15) must be returned
-(print-point (midpoint-segment (make-segment (make-point 10 20)
-                                             (make-point 50 10))))
+(define (area-rectangle rectangle) ())
+(define (perimeter-rectangle rectangle) ())
