5f293e4e4552ac50ae497758ca3906245f1e1f35
Alexander <lapseofreason17@gmail.com>
2021-10-17 23:34:48 +0300

    Update 03.scm

--- Exercises/2/03.scm
+++ Exercises/2/03.scm
@@ -1,13 +1,56 @@
-(define (make-rectangle-1 upper-segment lower-segment) (cons upper-segment lower-segment))
-(define (make-rectangle-2 left-segment right-segment) (cons left-segment right-segment))
+;      __
+;left |  |
+;     |__|
+;     lower
+(define (make-rectangle-1 left-segment lower-segment) (cons left-segment lower-segment))
+(define (first-rectangle rectangle) (car rectangle))
+(define (second-rectangle rectangle) (cdr rectangle))
+
+; upper
+;  __
+; |  | right
+; |__|
+(define (make-rectangle-2 upper-segment right-segment) (cons upper-segment right-segment))
+(define (first-rectangle rectangle) (car rectangle))
+(define (second-rectangle rectangle) (cdr rectangle))
 
 (define (make-segment start end) (cons start end))
-(define (start-segment segment) (car segment))
+(define (start-segment segment) (car segment));
 (define (end-segment segment) (cdr segment))
 
 (define (make-point x y) (cons x y))
 (define (x-point point) (car point))
 (define (y-point point) (cdr point))
 
-(define (area-rectangle rectangle) ())
-(define (perimeter-rectangle rectangle) ())
+(define (perimeter-rectangle rectangle) (* (+ (abs (- (y-point (start-segment (first-rectangle rectangle)))
+                                                      (y-point (end-segment (first-rectangle rectangle)))))
+                                              (abs (- (x-point (start-segment (second-rectangle rectangle)))
+                                                      (x-point (end-segment (second-rectangle rectangle))))))
+                                           2))
+                                       
+(define (area-rectangle rectangle) (* (abs (- (y-point (start-segment (first-rectangle rectangle)))
+                                              (y-point (end-segment (first-rectangle rectangle)))))
+                                      (abs (- (x-point (start-segment (second-rectangle rectangle)))
+                                              (x-point (end-segment (second-rectangle rectangle)))))))
+
+;Testing                                  
+;y
+;^
+;|
+;|___
+;|   |
+;|___|_______>x
+(define my-rectangle-1 (make-rectangle-1 (make-segment (make-point 0 2) (make-point 0 0))
+                                           (make-segment (make-point 0 0) (make-point 3 0))))
+(define my-rectangle-2 (make-rectangle-2 (make-segment (make-point 0 2) (make-point 0 0))
+                                           (make-segment (make-point 0 0) (make-point 3 0))))
+
+(display "P1 = ")
+(display (perimeter-rectangle my-rectangle-1))
+(display " , P2 = ")
+(display (perimeter-rectangle my-rectangle-2))    
+(newline)
+(display "S1 = ")
+(display (area-rectangle my-rectangle-1))
+(display " , S2 = ")
+(display (area-rectangle my-rectangle-2))  
