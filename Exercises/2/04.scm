e5b7ff0ac4f855785bf43eb7b47ce70d7b9551c8
Alexander <lapseofreason17@gmail.com>
2021-10-18 00:50:04 +0300

    Create 04.scm

---
+++ Exercises/2/04.scm
@@ -0,0 +1,8 @@
+;For this representation, verify that (car (cons x y)) yields x for any objects x and y.
+(define (cons x y)
+  (lambda (m) (m x y)))
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
