c8b06474eb20e0375b92c4c10b5e9475484c040f
Alexander <lapseofreason17@gmail.com>
2021-10-19 13:42:46 +0300

    Update 04.scm

--- Exercises/2/04.scm
+++ Exercises/2/04.scm
@@ -6,3 +6,13 @@
 
 (define (cdr z)
   (z (lambda (p q) q)))
+
+;Testing
+(define my-pair (cons (car (cons 0 1)) (cdr (cons 2 3))))
+;must be 0
+(display (car my-pair))
+
+(newline)
+
+;must be 3
+(display (cdr my-pair))
