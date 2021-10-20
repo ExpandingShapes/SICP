d279a42522e78ab4f27a5aaa557b727f073e8b80
Alexander <lapseofreason17@gmail.com>
2021-10-20 15:42:41 +0300

    Create 08.scm

---
+++ Exercises/2/08.scm
@@ -0,0 +1,32 @@
+(define (make-interval a b) (cons a b))
+(define (upper-bound interval) (max (car interval) (cdr interval)))
+(define (lower-bound interval) (min (car interval) (cdr interval)))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+             
+;describe how the difference of two intervals may be computed
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+;
+             
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+  (make-interval (min p1 p2 p3 p4)
+                 (max p1 p2 p3 p4))))
+             
+(define (div-interval x y)
+  (mul-interval
+  x
+  (make-interval (/ 1.0 (upper-bound y))
+                 (/ 1.0 (lower-bound y)))))
+             
+;Test
+;must be 3...5
+(display (sub-interval (make-interval 6 7)
+                       (make-interval 2 3)))
