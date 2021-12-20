094f4ac75dc70ab0a1902d35527e69212dd824a9
Alexander <lapseofreason17@gmail.com>
2021-12-20 23:01:00 +0300

    Create 10.scm

---
+++ Exercises/2/10.scm
@@ -0,0 +1,37 @@
+(define (make-interval a b) (cons a b))
+(define (upper-bound interval) (max (car interval) (cdr interval)))
+(define (lower-bound interval) (min (car interval) (cdr interval)))
+
+(define (width interval) (/ (- (upper-bound interval)
+                               (lower-bound interval))
+                           2))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+             
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+             
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+  (make-interval (min p1 p2 p3 p4)
+                 (max p1 p2 p3 p4))))
+             
+(define (div-interval x y)
+  (if (< (lower-bound y) 0)
+      (error "Dividing by an interval that spans zero is prohibited!")
+      (mul-interval
+       x
+       (make-interval (/ 1.0 (upper-bound y))
+                      (/ 1.0 (lower-bound y))))
+      )
+  )
+             
+;Tests
+(display (div-interval (make-interval 1 5)
+                       (make-interval (- 2) 2)))
